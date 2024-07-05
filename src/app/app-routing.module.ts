dd58cfe5137dc93cbb608865191435d99bcdc2e3
Rakesh Kumar Kuna <kunarakeshkumar@gmail.com>
2024-07-05 06:50:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { Component, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorksComponent } from './works/works.component';
+import { ContactComponent } from './contact/contact.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'Home',component:HomeComponent},
+  {path:'About',component:AboutComponent},
+  {path:'Works',component:WorksComponent},
+  {path:'Contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorksComponent } from './works/works.component';
+import { ContactComponent } from './contact/contact.component';
+import { NgxFastMarqueeModule } from 'ngx-fast-marquee';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    WorksComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    NgxFastMarqueeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
