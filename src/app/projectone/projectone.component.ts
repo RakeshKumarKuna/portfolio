3ea9506b805d3bb5dd3c28e02b17682d7b3a4073
Rakesh <kunarakeshkumar@gmail.com>
2025-12-04 23:20:15 +0530

    upgraded to angular-19

--- src/app/projectone/projectone.component.ts
+++ src/app/projectone/projectone.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-projectone',
-  templateUrl: './projectone.component.html',
-  styleUrls: ['./projectone.component.css']
+    selector: 'app-projectone',
+    templateUrl: './projectone.component.html',
+    styleUrls: ['./projectone.component.css'],
+    standalone: false
 })
 export class ProjectoneComponent {
 
--- src/app/projecttwo/projecttwo.component.ts
+++ src/app/projecttwo/projecttwo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-projecttwo',
-  templateUrl: './projecttwo.component.html',
-  styleUrls: ['./projecttwo.component.css']
+    selector: 'app-projecttwo',
+    templateUrl: './projecttwo.component.html',
+    styleUrls: ['./projecttwo.component.css'],
+    standalone: false
 })
 export class ProjecttwoComponent {
 
